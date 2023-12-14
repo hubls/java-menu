c3b3530acb4b2edcea62f16d771d0afb75069a1f
leesangjun <hubls@naver.com>
2023-12-14 21:16:36 +0900

    test3

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -2,7 +2,6 @@ package menu;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("you");
+        // TODO: 프로그램 구현
     }
 }
